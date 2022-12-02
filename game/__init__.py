99394bc069a1b72917d7169ef5f21d0dc0bde12b
Jason Estey <esteyj@yahoo.com>
2022-12-02 13:31:26 -0800

    load and draw player sprite

--- game/__init__.py
+++ game/__init__.py
@@ -2,7 +2,9 @@
 
 import pygame
 from . import input_quit
+from . import sprites
 
 __all__ = ["config", "input", "render"]
 
 pygame.init()
+sprites.load_textures()
--- game/render.py
+++ game/render.py
@@ -1,5 +1,6 @@
 import pygame
 from . import config
+from . import sprites
 
 
 def create_surface(caption):
@@ -11,6 +12,11 @@ def draw_frame(surface, world):
     """render the given game sim world to the given graphics surface"""
     surface.fill(world.background_colour)
 
-    # TODO: surface.blit something
+    draw_texture(surface, "player")
 
     pygame.display.flip()
+
+
+def draw_texture(surface, texture_name):
+    texture = sprites.get_texture(texture_name)
+    surface.blit(texture, texture.get_rect())
